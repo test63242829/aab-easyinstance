794aebe39f1e459a6c57d2540fc76dea90044c2f
fadilameen <fadilameen63@gmail.com>
2025-04-11 12:19:50 +0530

    odoo17

---
+++ demo_module/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Demo Module",
+    "version": "17.0.1.0.0",
+    'sequence': 1,
+    "summary": "A simple demo module for Odoo 17",
+    "category": "Tools",
+    "author": "Your Name",
+    "website": "http://www.example.com",
+    "license": "LGPL-3",
+    "depends": [],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
